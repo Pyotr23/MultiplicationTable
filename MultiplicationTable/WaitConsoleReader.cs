ba07aa06c73b1c55c278828facc2c9a74ae627b3
Pyotr <2chilavert3@mail.ru>
2021-01-01 20:04:36 +0300

    Изменена логика ожидания результата в консоли.

---
+++ MultiplicationTable/WaitConsoleReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    public class WaitConsoleReader
+    {
+        public static string ReadLine(int timeOutMillisecs = Timeout.Infinite)
+        {
+            var readLineTask = Task.Run(() => Console.ReadLine());
+            var isReceivedAnswer = readLineTask.Wait(timeOutMillisecs);
+            return isReceivedAnswer
+                ? readLineTask.Result
+                : null;
+        }
+    }
+}
